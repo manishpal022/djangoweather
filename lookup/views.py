6d39e7217d9b00914cc6bcef416481661fd32d2c
Manish <manishpal.055@gmail.com>
2020-09-01 14:03:47 +0530

    Final commit

--- lookup/views.py
+++ lookup/views.py
@@ -2,7 +2,32 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    import json
+    import requests
+    # http://api.openweathermap.org/data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q=Gwalior
+
+    if request.method == "POST":
+        zipcode = request.POST['zipcode']
+        api_requests = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode="+zipcode+"&distance=5&API_KEY=7F276637-93A1-4CB9-9DC0-F21094BB712B")
+        try:
+            api = json.loads(api_requests.content)
+            # print(api)
+        except Exception as e:
+            api = "Error..."
+        return render(request, 'home.html', {'api': api, 'category_color': api[0]['Category']['Name'].replace(" ", "").lower()})
+
+    else:
+        api_requests = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=5&API_KEY=7F276637-93A1-4CB9-9DC0-F21094BB712B")
+
+        # http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=5&API_KEY=7F276637-93A1-4CB9-9DC0-F21094BB712B
+
+        try:
+            api = json.loads(api_requests.content)
+            # print(api)
+        except Exception as e:
+            api = "Error..."
+
+        return render(request, 'home.html', {'api': api, 'category_color': api[0]['Category']['Name'].replace(" ", "").lower()})
 
 
 def about(request):
