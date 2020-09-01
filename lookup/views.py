5d7def4cac88549752438d42617c60dbb59ecf07
Manish <manishpal.055@gmail.com>
2020-09-01 14:29:23 +0530

    add some funtionality to handle error commit

--- lookup/views.py
+++ lookup/views.py
@@ -4,30 +4,22 @@ from django.shortcuts import render
 def home(request):
     import json
     import requests
-    # http://api.openweathermap.org/data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q=Gwalior
 
     if request.method == "POST":
         zipcode = request.POST['zipcode']
         api_requests = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode="+zipcode+"&distance=5&API_KEY=7F276637-93A1-4CB9-9DC0-F21094BB712B")
-        try:
-            api = json.loads(api_requests.content)
-            # print(api)
-        except Exception as e:
-            api = "Error..."
-        return render(request, 'home.html', {'api': api, 'category_color': api[0]['Category']['Name'].replace(" ", "").lower()})
 
     else:
         api_requests = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=5&API_KEY=7F276637-93A1-4CB9-9DC0-F21094BB712B")
 
-        # http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=5&API_KEY=7F276637-93A1-4CB9-9DC0-F21094BB712B
+    try:
+        api = json.loads(api_requests.content)
+        category_color = api[0]['Category']['Name'].replace(" ", "").lower()
+    except Exception as e:
+        api = "Error..."
+        category_color = 'No Output'
 
-        try:
-            api = json.loads(api_requests.content)
-            # print(api)
-        except Exception as e:
-            api = "Error..."
-
-        return render(request, 'home.html', {'api': api, 'category_color': api[0]['Category']['Name'].replace(" ", "").lower()})
+    return render(request, 'home.html', {'api': api, 'category_color': category_color})
 
 
 def about(request):
